d957e7fbe6cd55983c897a6bff6cbdde7b011b24
Grzegorz Kastelik <g.kastelik@gmail.com>
2020-08-16 17:53:47 +0200

    change lang

--- resources/lang/pl/validation.php
+++ resources/lang/pl/validation.php
@@ -111,11 +111,9 @@ return [
             'rule-name' => 'custom-message',
         ],
 		'g-recaptcha-response' => [
-			'required' => 'Please verify that you are not a robot.',
-			'captcha' => 'Captcha error! try again later or contact site admin.',
+			'required' => 'Zweryfikuj, że nie jesteś robotem.',
+			'captcha' => 'Bład Captcha! Spróbuj ponownie lub skontaktuj się z administratorem strony.',
 		],
     ],
-
     'attributes' => [],
-
 ];
